184978915c3fa3fcfde9561ef3003df2136039bd
Emma <ebelinge@msu.edu>
2026-03-18 16:26:57 +0000

    first 2 routes of 9.3

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/products", to: "products#index"
+  post "/products", to: "products#create"
 end
