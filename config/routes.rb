72a97a1addcbe1d6ad036766dd2013a30540f1d9
Emma <ebelinge@msu.edu>
2026-03-21 00:11:06 +0000

    completed 9

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/products", to: "products#index"
-  post "/products", to: "products#create"
+  resources :products
 end
